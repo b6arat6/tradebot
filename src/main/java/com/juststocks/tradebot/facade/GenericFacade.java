f97b3f4de429643acae949a4ecdee73018163cb0
b6arat6 <b6arat6@gmail.com>
2017-07-10 03:15:26 +0530

    generic facade

---
+++ src/main/java/com/juststocks/tradebot/facade/GenericFacade.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.juststocks.tradebot.facade;
+
+/**
+ * @author bharath_kandasamy
+ *
+ */
+public interface GenericFacade {
+
+	boolean login();
+	
+	boolean authenticate();
+	
+}
---
+++ src/main/java/com/juststocks/tradebot/facade/KCFacade.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.juststocks.tradebot.facade;
+
+/**
+ * @author bharath_kandasamy
+ *
+ */
+public class KCFacade implements GenericFacade {
+
+	@Override
+	public boolean login() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean authenticate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
