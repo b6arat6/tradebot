1546b0e7e3a0abae6c0d6cf45602827212619c9e
b6arat6 <b6arat6@gmail.com>
2017-07-10 02:59:20 +0530

    first commit

---
+++ src/main/java/com/juststocks/tradebot/TradebotApplication.java
@@ -0,0 +1,12 @@
+package com.juststocks.tradebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradebotApplication.class, args);
+	}
+}
